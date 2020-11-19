38408775524d2c8b460e80aa1cc1c6205992b939
9A713030 <72920522+9A713030@users.noreply.github.com>
2020-11-19 19:49:53 +0800

    Add files via upload

---
+++ test20201015/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test20201015
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //result = MessageBox.Show(message, caption, buttons);
+            int randomNum = new Random().Next(51)+1; //0-100
+            MessageBox.Show($"{randomNum}", "random Num", MessageBoxButtons.OK);
+        }
+    }
+}
